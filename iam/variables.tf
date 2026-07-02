06507c4df73155f2921b28c70bce50d86fb55e37
Malli-WAS <haripriyadharshu@gmail.com>
2026-07-02 12:22:06 +0530

    Refactor Terraform configuration by separating EC2 and IAM resources into distinct directories

--- iam/variables.tf
+++ iam/variables.tf
@@ -1,15 +1,4 @@
 ########################
-# EC2 Variables
-########################
-
-variable "instance_name" {
-  default = "Terraform-EC2"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-########################
 # IAM Variables
 ########################
 
