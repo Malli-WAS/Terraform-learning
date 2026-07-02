8ebbf953cfaf156b3cda7b5a571682752931324d
Malli-WAS <haripriyadharshu@gmail.com>
2026-07-02 11:46:35 +0530

    Add IAM and EC2 outputs to outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,30 @@
+####################
+# IAM Outputs
+####################
+
+output "demo_user" {
+  value = aws_iam_user.demo_user.name
+}
+
+output "hari_user" {
+  value = aws_iam_user.hari.name
+}
+
+output "mallikaraja_user" {
+  value = aws_iam_user.mallikaraja.name
+}
+####################
+# EC2 Outputs
+####################
+
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
+}
+
+output "public_ip" {
+  value = aws_instance.ec2_instance.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.ec2_instance.private_ip
+}
